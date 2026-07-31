75f9c349c17725bef3c850cdd2815a61a71cef09
tieutieu1905 <tieutieu1905@gmail.com>
2026-07-31 10:49:37 +0700

    feat: assemble full app in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useTournamentStore } from './store/tournamentStore.js'
+import AppHeader from './components/AppHeader.vue'
+import CoupleList from './components/CoupleList.vue'
+import ScheduleBracket from './components/ScheduleBracket.vue'
+import ResultsSummary from './components/ResultsSummary.vue'
+import RulesSection from './components/RulesSection.vue'
+import AdminPanel from './components/AdminPanel.vue'
+
+const store = useTournamentStore()
+onMounted(() => store.init())
+</script>
+
 <template>
   <div class="min-h-screen bg-slate-50 text-slate-900">
-    <h1 class="p-4 text-xl font-bold">Đồng Lươn Badminton Cup 2026</h1>
+    <AppHeader />
+    <main>
+      <CoupleList />
+      <ScheduleBracket />
+      <ResultsSummary />
+      <RulesSection />
+      <AdminPanel />
+    </main>
   </div>
 </template>
